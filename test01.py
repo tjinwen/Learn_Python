3d5b16fbf2f2bc9d0a716b6418f91451b19cee1e
tjinwen <tjinwen94128@gmail.com>
2016-03-24 00:14:15 +0800

    third test

--- test01.py
+++ test01.py
@@ -7,3 +7,10 @@ sum = 0
 for x in range(101):
 	sum = sum + x
 print(sum)
+
+#------------------------
+#peplo = ['tong','jin','wen']
+#scores = [95,75,85]
+
+NameS = {'tong':95,'jin':93,'wen':45}
+
