25c1616c8628464afdfb44a3759ab682ec1b6299
tjinwen <tjinwen94128@gmail.com>
2016-03-25 20:35:34 +0800

    learning test02 v1.2

--- test02.py
+++ test02.py
@@ -28,5 +28,18 @@ print("def my_abs(-1): ",my_abs(-1))
 def nop():
 	pass
 
+print("------------return more---------")
 
+import math
+
+def move(x,y,step,angle=0):
+	nx = x + step * math.cos(angle)
+	ny = y - step * math.sin(angle)
+	return nx,ny
+
+x,y = more(100,100,60,math.pi/6)
+print("x,y = more(100,100,60,math.pi/6): "x,y)
+
+r = more(100,100,60,math.pi/6)
+print("r = more(100,100,60,math.pi/6): ",r)
 
