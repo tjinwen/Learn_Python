0746756723a2cb89fece85a2cfb9013c66a12183
tjinwen <tjinwen94128@gmail.com>
2016-03-25 20:13:31 +0800

    learning test02 v0.1

---
+++ test02.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#----test02-------
+
+print("abs(-250): ",abs(-250))
+print("max(2,6,8): ",max(2,6,8))
+
+print("int('123'): ",int('123'))
+print("str(1.23): ",str(1.23))
+
+a = abs
+print("a=abs,a(-1): ",a(-1))
+
+
