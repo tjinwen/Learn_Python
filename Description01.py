7b3f3e96631fbf74148f0aedb6cab758fc513119
tjinwen <tjinwen94128@gmail.com>
2016-04-05 23:33:13 +0800

    problem01

--- Description01.py
+++ Description01.py
@@ -3,7 +3,11 @@
 
 #----Description1-------
 print("**************01***************")
-def add2num(x,y):
-	return x+y
-	
-if()
+#######input输入的数是str类型的，int和str不能进行转换
+#######type()函数可以判断数据类型	
+while True:
+	x=input()
+	y=input()
+	if(int(y)>=0 and int(y)<=10 and int(x)>=0 and int(x)<=10):
+		print(int(x)+int(y))
+		break
