b723f175fe82e189f1dbb4a3eccbf3980e64403b
tjinwen <tjinwen94128@gmail.com>
2016-04-05 23:33:36 +0800

    just test

---
+++ test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+#----test-------
+print(isinstance(3,int))
+
+print(type('a'))
+
+#b=input()
+#print(type(b)==int)
+
+print('------------------------------')
+
+a='abc'
+print(" ",a[::1])
+print(a[::-1])
+
+print('-------------------------------')
+L = [('Bob', 75),('Adam', 92),('Bart', 66),('Lisa', 88)]
+L2=('tong',89)
+l3=L2[0].lower()
+print(l3)
